6a10c3a9dd239a9de9c678a2ae9fb3696e17546e
Heather King <hkingdevelopment@gmail.com>
2021-11-02 18:16:51 +0000

    adds save function to album_repository
    can now create and save albums to the album table, linking to the artist id

--- console.py
+++ console.py
@@ -1,10 +1,15 @@
 import pdb
+from models.album import Album
 from models.artist import Artist
+from repositories import album_repository
 
 import repositories.artist_repository as artist_repository
 
 
 artist_1 = Artist("Johnny", "Cash")
 artist_repository.save(artist_1)
+
+album_1 = Album("I Walk The Line", "Rockabilly", artist_1)
+album_repository.save(album_1)
 
 pdb.set_trace()
--- db/music_library.sql
+++ db/music_library.sql
@@ -1,9 +1,18 @@
-DROP TABLE artists;
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
 
 CREATE TABLE artists (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255)
 );
+
+CREATE TABLE albums (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    genre VARCHAR(255),
+    artist_id INT REFERENCES artists(id)
+)
 
 -- INSERT INTO artists (first_name, last_name) VALUES ("Eva", "Cassidy");
